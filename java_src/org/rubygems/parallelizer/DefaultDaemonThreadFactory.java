52b3191a5f8f6a432be94f4d3d278ef740a2b80d
Mohamed Hafez <mohamed.m.m.hafez@gmail.com>
2020-07-16 15:23:09 -0700

    documentation of why we made this class, and what to compare it to

--- java_src/org/rubygems/parallelizer/DefaultDaemonThreadFactory.java
+++ java_src/org/rubygems/parallelizer/DefaultDaemonThreadFactory.java
@@ -2,6 +2,9 @@ package org.rubygems.parallelizer;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
+// compare this class to defaultDaemonThreadFactory, whose source you can see at
+// https://github.com/AdoptOpenJDK/openjdk-jdk11u/blob/5f01925b80ed851b133ee26fbcb07026ac04149e/src/java.base/share/classes/java/util/concurrent/Executors.java#L612-L637
+// the only changes are lines 27-28: making the thread a deamon thread
 public class DefaultDaemonThreadFactory implements ThreadFactory {
 	static final AtomicInteger poolNumber = new AtomicInteger(1);
 	final ThreadGroup group;
